a21edd4be47bc4708425e2d4ecf08e616fe3602a
edyi <45528050+edyi@users.noreply.github.com>
2019-11-23 00:56:40 +0900

    Create git_install.sh

---
+++ git_install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+yum install -y wget make
+yum install -y sqlite-devel bzip2-devel xz-libs gcc g++ build-essential kernel-headers kernel-devel
+cd /usr/local/src
+wget https://www.kernel.org/pub/software/scm/git/git-2.24.0.tar.gz 
+tar zxvf git-2.24.0.tar.gz
+yum install -y gcc curl-devel expat-devel gettext-devel openssl-devel zlib-devel perl-ExtUtils-MakeMaker
+cd git-2.24.0
+make prefix=/usr/local all
+make prefix=/usr/local install
